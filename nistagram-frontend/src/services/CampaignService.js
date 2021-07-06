564b62a0ad8d8c87c0a2185792fa14d63e4e4623
nikolinaapetrovic <nikolinaapetrovic97@gmail.com>
2021-07-06 15:54:27 +0200

    [NF-08] Agent CRUD added (#16)

--- nistagram-frontend/src/services/CampaignService.js
+++ nistagram-frontend/src/services/CampaignService.js
@@ -2,19 +2,32 @@ import http from "../http-camp";
 
 class CampaignService {
   save(post) {
-    return http.post("/", post);
+    return http.post("/campaign/", post);
   }
   delete(post) {
-    return http.delete(`/${post}`);
+    return http.delete(`/campaign/${post}`);
   }
   findByUsername(username) {
-    return http.get(`/${username}`);
+    return http.get(`/campaign/${username}`);
   }
   findById(id) {
-    return http.get(`/id/${id}`);
+    return http.get(`/campaign/id/${id}`);
   }
   myCampaign(id) {
-    return http.get(`/${id}/mycampaign`);
+    return http.get(`/campaign/${id}/mycampaign`);
+  }
+
+  saveAgent(post) {
+    return http.post("/agent/", post);
+  }
+  requests() {
+    return http.get(`/agent/requests`);
+  }
+  agents() {
+    return http.get(`/agent/agents`);
+  }
+  approveAgent(id) {
+    return http.post(`/agent/approve/${id}`);
   }
 }
 
